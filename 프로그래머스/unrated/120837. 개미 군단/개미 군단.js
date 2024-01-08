849a8ea62108d2835deeb91a3fc86f439587bb04
soo <126536384+soooee4@users.noreply.github.com>
2024-01-08 19:28:58 +0900

    [level unrated] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -1,9 +1,14 @@
 function solution(hp) {
-    if (hp % 5 === 0) {
-        return Math.floor(hp / 5);
-    } else if (hp % 5 === 1 || hp % 5 === 3) {
-        return Math.floor(hp / 5) + 1;
+    const hp계산 = hp % 5;
+    const 오개미 = parseInt(hp / 5);
+    let 삼개미 = 0;
+    let 일개미 = 0;
+    
+    if(hp계산 < 3) {
+        일개미 = hp계산;
     } else {
-        return Math.floor(hp / 5) + 2;
+        삼개미 = 1;
+        일개미 = hp계산 - 3;
     }
+    return 오개미 + 삼개미 + 일개미;
 }
